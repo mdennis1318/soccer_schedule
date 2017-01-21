f580c98e562e128f98b5b49b1e65922782986336
Matt Dennis <dennis57@msu.edu>
2017-01-21 03:17:36 -0500

    we figured out how to import the teams

--- Soccer Scheduling.py
+++ Soccer Scheduling.py
@@ -4,20 +4,32 @@ Soccer Scheduling
 
 import http.client
 import json
+from teams import *
 
 connection = http.client.HTTPConnection('api.football-data.org')
-headers = { 'X-Auth-Token': 'a49c78a50c0e486ca40791f1c9323b12', 'X-Response-Control': 'minified' }
+headers = { 'X-Auth-Token': 'a49c78a50c0e486ca40791f1c9323b12', \
+           'X-Response-Control': 'minified' }
 
 def get_schedule(time_int, n_p_bool, team):
-    #connection.request('GET', '/v1/competitions', None, headers )
-    #response = json.loads(connection.getresponse().read().decode())
-    connection.request('GET', '/v1/teams/66/fixtures?timeFrame=n14', None, headers )
+    
+    #decide if the time frame is future(true) or past(false)
+    if n_p_bool:
+        t_f_str = "n" #future
+    else:
+        t_f_str = "p" #past
+    
+    #build path of which fixtures to get
+    path_str = "/v1/teams/" + str(team) + "/fixtures?timeFrame=" \
+                + t_f_str + str(time_int)
+    
+    
+    connection.request('GET', path_str, None, headers )
     response2 = json.loads(connection.getresponse().read().decode())
     
     return response2
 
-#print(response2["fixtures"][0]["awayTeamId"])
+response2 = get_schedule(14, True, 66)
 
-response2 = get_schedule(1,1,1)
+print(response2["fixtures"][0]["awayTeamId"])
 
-print (response2)
+#print (response2)
